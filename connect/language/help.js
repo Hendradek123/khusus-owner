0b4b9819cf9e32bdae93c20e00d5b08138f9445f
DanzGantevk75 <102861930+DanzGantevk75@users.noreply.github.com>
2022-06-26 01:38:06 +0700

    Update help.js

--- connect/language/help.js
+++ connect/language/help.js
@@ -1,99 +1,20 @@
 exports.help = (prefix, author, botname, owner) => {
 return `
-_*=>=>=>=>=>=>=>=>=>=>=>*_
-*( BOT INI KHUSUS OWNER UNTUK JUAL/BELI BOT! )*
-_*=>=>=>=>=>=>=>=>=>=>=>*_
-❑ _*MY SERVER OWNER INFO*_
-*× Owner Name : ${global.author}*
-*× Owner Name: ${global.botname}*
-*× My Owner : ${global.owner}*
-*× My Team : Danzz Team*
-_*=>=>=>=>=>=>=>=>=>=>=>*_
+*_Halo Kak, Saya Owner Sekaligus Pembuat DanzBot_*
+Jadi Saya Ini Bukan Bot Dan Tidak Pula Memiliki Fitur
+Seperti Bot, Ini Bot Khusus Owner Saya Buat Hanya Untuk Jualan
+Jika Kakak Ingin SewaBot/JasaRun Tinggal PC Saya Kak!
+Liat Harga SewaBot :
+1 minggu : 5k
+1 bulan : 10k
+Permanen : 20k
 
-*∆ [ SIMPEL OWNER MENU ] ∆*
+Liat Harga JasaRun :
+20k Tanpa Ada Kadaluarsa! 
 
-❑ *OTHERS MENU*
-• #script
-• #ping
-• #limit
-• #ceklimit
-• #speed
-• #status
-• #botstatus
-• #runtime
+Ini Nomor Bot Saya Jika Ingin Tes Fitur! 
+https://wa.me/62882708632799
 
-❑ *GROUP MENU*
-• #kick
-• #add
-• #promote
-• #demote
-• #setname
-• #setdesc
-• #setppgrup
-• #tagall
-• #hidetag
-• #vote
-• #upvote
-• #devote
-• #cekvote
-• #delvote
-• #group
-• #editinfo
-• #antilink
-
-❑ *OWNER MENU*
-• #join
-• #leave
-• #getcase
-• #creategc
-• #block
-• #unblock
-• #bcall
-• #bcgc
-• #broadcast
-• #prem del 628xxxx
-• #prem add 628xxxx
-
-❑ *FUN MENU*
-• #jadian
-• #jodohku
-• #tictactoe
-• #delttt
-• #family100
-• #tebak
-• #math
-• #suitpvp
-
-❑ *CONVERT MENU*
-• #tahta
-• #teksto
-• #sticker
-• #toimage
-• #tovideo
-• #tomp3
-
-❑ *NULIS MENU*
-• #nulis
-• #nuliskiri
-• #nuliskanan
-• #foliokiri
-• #foliokanan
-
-❑ *ANIME MENU*
-• #nekonime
-• #randomloli
-• #randomwaifu
-• #randomneko
-• #randomanime
-
-❑ *DOWNLOAD MENU*
-• #play
-• #ytplay
-• #ytmp3
-• #ytmp4
-• #tiktok
-• #tiktokmp4
-• #tiktokmp3
-• #mediafire
+Jika Owner Belum Membalas Chat Kakak Mohon Di tunggu Ya Mungkin Lagi Sibuk! 
 `
 }
